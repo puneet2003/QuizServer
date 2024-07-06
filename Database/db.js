8d99da03b8034f3663055baff8a692d3156ef5e0
puneet2003 <puneetkumar322003@gmail.com>
2024-07-07 00:16:03 +0530

    second

--- Database/db.js
+++ Database/db.js
@@ -1,8 +1,8 @@
 const User = require("../Models/User")
 const mongoose = require("mongoose");
 const Connection = async () => {
-    // const URL = "mongodb://127.0.0.1:27017/Puneet";
-    const URL = "mongodb+srv://triprabhat__001:nXDTCmoV8gNammwq@quiz-app.tckswf5.mongodb.net";
+    const URL = "mongodb://127.0.0.1:27017/Puneet";
+    // const URL = "mongodb+srv://triprabhat__001:nXDTCmoV8gNammwq@quiz-app.tckswf5.mongodb.net";
     try {
         const db = await mongoose.connect(URL);
         console.log('Database connected successfully');
