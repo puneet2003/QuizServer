e8144f753081340a1851d95c465b0ab8e9a146bb
puneet2003 <puneetkumar322003@gmail.com>
2024-09-06 03:42:55 +0530

    database update

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,8 @@ const User = require("../Models/User")
 const mongoose = require("mongoose");
 const Connection = async () => {
     // const URL = "mongodb://127.0.0.1:27017/quiz29";
-    const URL = "mongodb+srv://puneetk322003:JPIDxnO99fxRdeUw@cluster0.clvpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    // const URL = "mongodb+srv://puneetk322003:JPIDxnO99fxRdeUw@cluster0.clvpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    const URL = "mongodb+srv://puneetk322003:JPIDxnO99fxRdeUw@cluster0.clvpk.mongodb.net"
     // const URL = "mongodb+srv://triprabhat__001:nXDTCmoV8gNammwq@quiz-app.tckswf5.mongodb.net";
     try {
         const db = await mongoose.connect(URL);
